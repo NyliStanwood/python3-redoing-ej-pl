ba0a0ebcfc23467ead18d09318a85367fc0e7780
Marilyn Stanwood <nyliz@live.com>
2017-10-07 15:07:12 -0400

    Set tests sample

---
+++ set_simple_example.py
@@ -0,0 +1,34 @@
+#set s
+s = set([ 1, 2, 3 ])
+
+#set t
+t = set([ 3, 4, 5 ])
+
+#union WONT REPEAT '3' ,
+union = s.union(t)
+
+#intersection
+inter = s.intersection(t)
+
+#difference
+
+dif_s = d.difference(t)
+
+dif_t = t.difference(s)
+
+#set are mre efficient (but elemnets cannot be repeated)
+# efficient to find element in a set
+
+#find out if a set will order itself at the moment of create it
+
+#returns true
+1 in s
+
+#returns false
+1 in t
+
+#returns false
+1 not in s
+
+#returns true
+1 not in t
